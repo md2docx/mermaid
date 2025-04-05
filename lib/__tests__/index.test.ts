344daa60dca916040a10942b176bf03dda1bfb94
Mayank <mayank.srmu@gmail.com>
2025-04-05 12:49:18 +0530

    Clean up

--- lib/__tests__/index.test.ts
+++ lib/__tests__/index.test.ts
@@ -2,7 +2,6 @@ import { describe, it, vi } from "vitest";
 import { toDocx } from "@m2d/core"; // Adjust path based on your setup
 import { unified } from "unified";
 import remarkParse from "remark-parse";
-import remarkGfm from "remark-gfm";
 import fs from "fs";
 import { mermaidPlugin } from "../src";
 
@@ -12,12 +11,7 @@ const markdown = fs.readFileSync("../sample.md", "utf-8");
  * concurrently run unit tests.
  */
 describe.concurrent("toDocx", () => {
-  const mdast = unified().use(remarkParse).use(remarkGfm).parse(markdown);
-
-  it("should handle mermaid", async ({ expect }) => {
-    const docxBlob = await toDocx(mdast, {}, { plugins: [mermaidPlugin()] });
-    expect(docxBlob).toBeInstanceOf(Blob);
-  });
+  const mdast = unified().use(remarkParse).parse(markdown);
 
   it("should not have any console.log", async ({ expect }) => {
     const consoleSpy = vi.spyOn(console, "log");
